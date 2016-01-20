db954157adb78b63ebe92ac610e7afb870b2e58b
Robby <robby@k-state.edu>
2016-01-20 08:37:21 -0600

    Initial commit.

---
+++ project/LogikaRuntimeBuild.scala
@@ -0,0 +1,87 @@
+/*
+Copyright (c) 2016, Robby, Kansas State University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import com.typesafe.sbteclipse.core.EclipsePlugin._
+import sbt.Keys._
+import sbt._
+
+object LogikaRuntimeBuild extends Build {
+  val scalaVer = "2.11.7"
+
+  lazy val logikaRuntime = (project in file(".")).
+    settings(
+      organization := "org.sireum",
+      name := "logika-runtime",
+      incOptions := incOptions.value.withNameHashing(true),
+      retrieveManaged := true,
+      version := "3.0.0",
+      scalaVersion := scalaVer,
+      scalacOptions ++= Seq("-target:jvm-1.8", "-Ybackend:GenBCode",
+        "-Ydelambdafy:method", "-feature", "-Xexperimental",
+        "-optimize", "-Yopt:l:classpath"),
+      scalacOptions in(Compile, doc) := Seq("-groups", "-implicits"),
+      parallelExecution in Test := true,
+      testOptions += Tests.Argument(TestFrameworks.JUnit, "-q", "-v"),
+      libraryDependencies ++= Seq(
+        "org.scala-lang" % "scala-reflect" % scalaVer,
+        "com.novocode" % "junit-interface" % "0.11"
+      ),
+      EclipseKeys.withSource := true,
+      EclipseKeys.relativizeLibs := true,
+      EclipseKeys.useProjectId := true,
+      EclipseKeys.withBundledScalaContainers := false,
+      EclipseKeys.eclipseOutput := Some("bin"),
+      publishMavenStyle := true,
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (isSnapshot.value)
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases" at nexus + "service/local/staging/deploy/maven2")
+      },
+      publishArtifact in Test := false,
+      pomIncludeRepository := { _ => false },
+      pomExtra :=
+        <url>https://github.com/sireum/v3-logika-runtime/</url>
+          <licenses>
+            <license>
+              <name>Simplified BSD License</name>
+              <url>https://github.com/sireum/v3-logika-runtime/blob/master/license.md</url>
+            </license>
+          </licenses>
+          <scm>
+            <url>git://github.com/sireum/v3-logika-runtime.git</url>
+            <connection>scm:git://github.com/sireum/v3-logika-runtime.git</connection>
+          </scm>
+          <developers>
+            <developer>
+              <id>robby-phd</id>
+              <name>Robby</name>
+              <url>http://people.cis.ksu.edu/~robby</url>
+            </developer>
+          </developers>
+    )
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sireum Logika Runtime Library
+
+This project provides a runtime library for the
+[Sireum Logika](http://logika.sireum.org) programming language.
