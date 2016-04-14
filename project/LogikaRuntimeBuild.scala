5f8c232a6109de8ba26a109adf2c116b3740f65a
Robby <robby@k-state.edu>
2016-04-14 08:56:10 -0500

    Released 3.0.0-4.

--- project/LogikaRuntimeBuild.scala
+++ project/LogikaRuntimeBuild.scala
@@ -38,7 +38,7 @@ object LogikaRuntimeBuild extends Build {
       name := "logika-runtime",
       incOptions := incOptions.value.withNameHashing(true),
       retrieveManaged := true,
-      version := "3.0.0-4-SNAPSHOT",
+      version := "3.0.0-5-SNAPSHOT",
       scalaVersion := scalaVer,
       scalacOptions in(Compile, doc) := Seq("-groups", "-implicits"),
       parallelExecution in Test := true,
